a77592159341d137d80ec0d512cfa7ef50452877
Julien Brun <brun.julien@gmail.com>
2018-03-14 12:24:00 -0700

    updated dustan script

--- docs/workshop_data_rescue/coral_Dustan.R
+++ docs/workshop_data_rescue/coral_Dustan.R
@@ -6,25 +6,28 @@ library(readxl)
 
 ## CONSTANTS ----
 meas_names <- c("transect", "data_id", "card_nb", "intercept_distance_cm","species_code",
-  "growth_form1","growth_form23","colony_color","stomodael_color", "M_laevis")
+  "growth_form1","growth_form23","colony_color","stomodael_color", "M_laevis", "species_name")
 
-coral_file <- "~/Data/coral-rescue/V_Transect-Data-Mannularis_Dancing-Lady-Reef_1972.xls"
+coral_path <- "/Users/brun/Data/coral-rescue"
+coral_filename <- "QC_Transect-Data-Mannularis_Dancing-Lady-Reef_1972.xlsx"
+coral_file <- file.path(coral_path,coral_filename)
 
 
 ## MAIN ----
 
+## Measurement table ##
+
 # Read the data
-coral_xls <- read_xls(coral_file, 
-                      sheet = 1, 
-                      skip = 21)
+coral_xls <- read_xlsx(coral_file, 
+                      sheet = "Data")
 
 
-# Drop the columns with summary stats
-coral_xls <- coral_xls %>% select(-(X__5:X__9), -starts_with("My work"))
+# Drop the extra columns with summary stats
+coral_xls <- coral_xls %>% select(-starts_with("X__"))
 
 
 # Extract measurement 1
-meas1 <- select(coral_xls, 1:10) 
+meas1 <- select(coral_xls, c(1:10,21)) 
 # Rename the columns
 names(meas1) <- meas_names
 
@@ -36,9 +39,8 @@ meas1 <- meas1 %>%
   mutate(measurement_number = 1) %>%
   select(-growth_form23)
 
-
 # Extract measurement 2
-meas2 <- select(coral_xls, c(1:3, 12:18))
+meas2 <- select(coral_xls, c(1:3, 12:18,22))
 # Rename the columns
 names(meas2) <- meas_names
 # Add column for measuremnt and split growth form
@@ -49,24 +51,52 @@ meas2 <- meas2 %>%
   mutate(measurement_number = 2) %>%
   select(-growth_form23)
 
-
 # Append the two dataframes
 coral_measurement <- rbind(meas1, meas2)
-  
+
 names(coral_measurement)
-# Some Cleaning
 # sort by transect
 coral_measurement <- coral_measurement %>%
   # sort data
-  arrange(transect,data_id,card_nb,measurement_number) %>%
+  arrange(transect, data_id, card_nb, measurement_number)
+
+
+## Species table ##
+# read
+species_codes <- read_xlsx(coral_file, 
+                      sheet = "Species_Codes") %>%
+  select(1:2) %>%
+  na.omit() 
+names(species_codes) <- c("key", "species_names")
+  
+# Join to measurement table
+coral_measurement <- coral_measurement %>%
+  # left_join(species_codes, by = c("species_code" = "key")) %>%
   # rearrange columns
-  select(transect,data_id,card_nb,measurement_number,intercept_distance_cm,species_code,
+  select(transect,data_id,card_nb,measurement_number,intercept_distance_cm,species_code,species_name,
          growth_form1,growth_form_2,growth_form_3,
          colony_color,stomodael_color,M_laevis)
-
-
-
-
-
   
   
+## Transects table ##
+transect_info <- read_xlsx(coral_file, 
+                            sheet = "Transects") %>%
+    na.omit() 
+
+## Growth table ##
+growth_info <- read_xlsx(coral_file, 
+                          sheet = "tidy_growth_form") 
+
+## Color table ##
+color_info <- read_xlsx(coral_file, 
+                        sheet = "tidy_color_codes") 
+
+
+
+## Write files
+write_csv(coral_measurement, "~/Data/coral-rescue/V_Transect-Data-Mannularis_Dancing-Lady-Reef_1972_measurements.csv")
+write_csv(species_codes, "~/Data/coral-rescue/V_Transect-Data-Mannularis_Dancing-Lady-Reef_1972_species_codes.csv")
+write_csv(species_codes, "~/Data/coral-rescue/V_Transect-Data-Mannularis_Dancing-Lady-Reef_1972_transects_info.csv")
+write_csv(species_codes, "~/Data/coral-rescue/V_Transect-Data-Mannularis_Dancing-Lady-Reef_1972_growth-form_codes.csv")
+write_csv(species_codes, "~/Data/coral-rescue/V_Transect-Data-Mannularis_Dancing-Lady-Reef_1972_color_codes.csv")
+
